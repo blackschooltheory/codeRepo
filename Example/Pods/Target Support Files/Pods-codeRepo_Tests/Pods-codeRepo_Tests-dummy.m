8c1ce8aad4b1d8f17d3902dee0a22b40bbd61793
blackschooltheory <1455801522@qq.com>
2021-07-15 22:10:49 +0800

    初始化

---
+++ Example/Pods/Target Support Files/Pods-codeRepo_Tests/Pods-codeRepo_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_codeRepo_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_codeRepo_Tests
+@end
---
+++ Example/Pods/Target Support Files/codeRepo/codeRepo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_codeRepo : NSObject
+@end
+@implementation PodsDummy_codeRepo
+@end
