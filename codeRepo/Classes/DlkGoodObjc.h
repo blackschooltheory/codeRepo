157e6df233f6d31b2424330df653cb3811e29e7d
blackschooltheory <1455801522@qq.com>
2021-07-16 07:41:53 +0800

    更新

--- codeRepo/Classes/DlkGoodObjc.h
+++ codeRepo/Classes/DlkGoodObjc.h
@@ -1,16 +1,16 @@
 //
-//  TestClass.h
+//  DlkGoodObjc.h
 //  codeRepo
 //
-//  Created by mac on 2021/7/15.
+//  Created by mac on 2021/7/16.
 //
 
 #import <Foundation/Foundation.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface TestClass : NSObject
--(void) getTestClassData;
+@interface DlkGoodObjc : NSObject
+-(void) goodname;
 @end
 
 NS_ASSUME_NONNULL_END
---
+++ codeRepo/Classes/DlkGoodObjc.m
@@ -0,0 +1,14 @@
+//
+//  DlkGoodObjc.m
+//  codeRepo
+//
+//  Created by mac on 2021/7/16.
+//
+
+#import "DlkGoodObjc.h"
+
+@implementation DlkGoodObjc
+-(void) goodname{
+    NSLog(@"0");
+}
+@end
