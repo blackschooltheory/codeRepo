e2938cedf9c60c12ea78e9c9f06cfc79e4f95515
blackschooltheory <1455801522@qq.com>
2021-07-15 23:06:38 +0800

    fix Config

---
+++ codeRepo/Classes/DogModel.h
@@ -0,0 +1,16 @@
+//
+//  DogModel.h
+//  codeRepo
+//
+//  Created by mac on 2021/7/15.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DogModel : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ codeRepo/Classes/DogModel.m
@@ -0,0 +1,12 @@
+//
+//  DogModel.m
+//  codeRepo
+//
+//  Created by mac on 2021/7/15.
+//
+
+#import "DogModel.h"
+
+@implementation DogModel
+
+@end
