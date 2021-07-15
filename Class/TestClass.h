6e82480ca1e52ea3ea4453c939ad1abfafda6a94
blackschooltheory <1455801522@qq.com>
2021-07-15 22:35:19 +0800

    first commit

---
+++ Class/TestClass.h
@@ -0,0 +1,16 @@
+//
+//  TestClass.h
+//  codeRepo
+//
+//  Created by mac on 2021/7/15.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TestClass : NSObject
+-(void) getTestClassData;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Class/TestClass.m
@@ -0,0 +1,14 @@
+//
+//  TestClass.m
+//  codeRepo
+//
+//  Created by mac on 2021/7/15.
+//
+
+#import "TestClass.h"
+
+@implementation TestClass
+-(void) getTestClassData{
+    NSLog(@"log 数据");
+}
+@end
