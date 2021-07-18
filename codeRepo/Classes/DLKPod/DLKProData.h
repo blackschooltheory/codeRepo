3fad140817649c9a867b57a6942c328c226fe3fe
blackschooltheory <1455801522@qq.com>
2021-07-18 17:00:20 +0800

    上传3.0数据改造

---
+++ codeRepo/Classes/DLKPod/DLKProData.h
@@ -0,0 +1,16 @@
+//
+//  DLKProData.h
+//  codeRepo
+//
+//  Created by mac on 2021/7/18.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DLKProData : NSObject
+-(void) someProw;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ codeRepo/Classes/DLKPod/DLKProData.m
@@ -0,0 +1,14 @@
+//
+//  DLKProData.m
+//  codeRepo
+//
+//  Created by mac on 2021/7/18.
+//
+
+#import "DLKProData.h"
+
+@implementation DLKProData
+-(void) someProw{
+    NSLog(@"我有一些超人力");
+}
+@end
